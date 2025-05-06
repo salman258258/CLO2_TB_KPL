48b0b6e1ac3788d2bec15cef6d72a8f91c280791
malbae <akmal67e@gmail.com>
2025-05-06 10:38:32 +0700

    Del

--- test_distance_converter.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from distance_converter import convert_distance
-
-class TestDistanceConverter(unittest.TestCase):
-    def test_km_to_m(self):
-        self.assertEqual(convert_distance(1, 'km', 'm'), 1000)
-    def test_m_to_km(self):
-        self.assertEqual(convert_distance(500, 'm', 'km'), 0.5)
